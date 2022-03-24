651cbae4fbf468a94e09f5d9d40d67610e79e1b2
Tiliksew <tiliksewmulugeta552@gmail.com>
2022-03-24 16:45:24 +0300

    MUI component library added

--- client/eprocurement/src/App.js
+++ client/eprocurement/src/App.js
@@ -1,3 +1,9 @@
 import React from "react";
-const ti= ()=><h1>Tiliksew</h1>
-export default ti
+import Button from '@mui/material/Button';
+
+function App() {
+  return <Button variant="contained">Hello World</Button>;
+}
+
+//const ti= ()=><h1>Tiliksew</h1>
+export default App
