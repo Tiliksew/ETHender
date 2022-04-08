5e3d81a8325ec63f1a5765789d83d5a98b118339
Tiliksew <tiliksewmulugeta552@gmail.com>
2022-04-08 11:20:37 +0300

    notificationbidform and feedback models added

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({limit:"30mb",extended:"true"}))
 const CONNECTIONI_URL='mongodb+srv://tiliksewmulueta:tiliksewmulueta@cluster0.zn5cq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const port=process.env.port || 5000
 mongoose.connect(CONNECTIONI_URL,{useNewUrlParser: true, useUnifiedTopology: true,})
-.then(()=>app.listen(port,()=>console.log(`listening successfully at port ${port}`)))
+.then(()=>app.listen(port,()=>console.log(`MongoDb conncted and listening successfully at port ${port}`)))
 .catch((error)=>console.log(error.message))
 // mongoose.set('useFindAndModify',false)c
 app.use('/tenders',route)
