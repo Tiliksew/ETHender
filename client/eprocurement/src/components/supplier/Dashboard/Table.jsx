082802c86fd9b7554a30fc2e90e8c5f5622aeb91
Tiliksew <tiliksewmulugeta552@gmail.com>
2022-04-29 14:10:51 +0300

    we can successfully fetch data from server to our react app

--- client/eprocurement/src/components/supplier/Dashboard/Table.jsx
+++ client/eprocurement/src/components/supplier/Dashboard/Table.jsx
@@ -11,6 +11,7 @@ import { FiDelete, FiEdit } from 'react-icons/fi';
 import { RiEditBoxFill } from 'react-icons/ri';
 import { FaEdit, FaRegEdit, FaUserEdit } from 'react-icons/fa';
 import { MdDelete } from 'react-icons/md';
+import axios from 'axios'
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -40,11 +41,19 @@ const rows = [
   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
   createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Cupcake', 305, 3.7, 67, 4.3), 
   createData('Gingerbread', 356, 16.0, 49, 3.9),
 ];
 
 export default function CustomizedTables() {
+
+ React.useEffect(() => {
+axios.get('https://jsonplaceholder.typicode.com/posts')
+  .then((response) => console.log(response.data))
+ 
+
+
+ }, []);
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 700 }} aria-label="customized table">
